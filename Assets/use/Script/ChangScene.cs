1fb6b8f2463dc3983ad55bc7734fb73a906018dc
ToTuKa <Kt08007921@gmail.com>
2025-09-24 16:01:09 +0900

    進展なし

--- Assets/use/Script/ChangScene.cs
+++ Assets/use/Script/ChangScene.cs
@@ -6,10 +6,17 @@ using UnityEngine.SceneManagement;
 public class ChangScene : MonoBehaviour
 {
     public string sceneName; //移行先のシーン
+    public float delayTime = 0.0f; //実行の遅延
 
     //シーン変更
     public void SceneChanger()
     {
+        StartCoroutine(DelayedSceneChange());
+    }
+
+    private IEnumerator DelayedSceneChange()
+    {
+        yield return new WaitForSeconds(delayTime);
         SceneManager.LoadScene(sceneName);
     }
 }
